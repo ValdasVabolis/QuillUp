0ca69bc8820a0e166f1d478915a9e5e724da8c69
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-07-11 18:24:06 +0000

    Added buttons in home for user post history and new post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
    def home
+      @users_posts = Users::Post.all
    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,27 @@
-users = (1..10).to_a.map do | i|
-    User.create({
-        email: "test#{i}@example.com",
-        password: "asdf1234",
-        password_confirmation: "asdf1234"
-    })
-end
-
-posts = [
-    { title: 'How do I tie my shoes', body: 'I need help' },
-    { title: 'How often do I need to brush my teeth?', body: 'Also whats some good toothpaste' },
-    { title: 'What do I eat for dinner', body: 'I am hungry' },
-    { title: 'Which tea do you prefer', body: 'I like green tea' },
-    { title: 'How do I improve my posture', body: 'I have a geek neck' }
-]
-
-users.each do |u|
-    posts.each do |h|
-        post = Users::Post.new(h)
-        post.user = u
-        post.save
+def seed_development
+    users = (1..10).to_a.map do | i|
+        User.create({
+            email: "test#{i}@example.com",
+            password: "asdf1234",
+            password_confirmation: "asdf1234"
+        })
+    end
+    
+    posts = [
+        { title: 'How do I tie my shoes', body: 'I need help' },
+        { title: 'How often do I need to brush my teeth?', body: 'Also whats some good toothpaste' },
+        { title: 'What do I eat for dinner', body: 'I am hungry' },
+        { title: 'Which tea do you prefer', body: 'I like green tea' },
+        { title: 'How do I improve my posture', body: 'I have a geek neck' }
+    ]
+    
+    users.each do |u|
+        posts.each do |h|
+            post = Users::Post.new(h)
+            post.user = u
+            post.save
+        end
     end
 end
+
+seed_development if Rails.env.development?
