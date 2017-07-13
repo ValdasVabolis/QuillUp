12af7974d91006f224e5515b46076ce5ec0d63b0
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-07-13 21:52:01 +0000

    WIP comment votes and some refactoring

--- app/assets/javascripts/users/posts/comments.js
+++ app/assets/javascripts/users/posts/comments.js
@@ -22,4 +22,26 @@ $(function() {
       }
     });
   });
+
+  $.each(['up', 'down'], function(i, o) {
+    $(document).on('click', "button.comment-vote-" + o, function() {
+      var el = $(this).closest(".comment-votes");
+      var id = el.data("id");
+      var url = "/users/posts/" + post_id + "comments/" + comment_id + "/vote/" + o;
+      return $.ajax({
+        type: "POST",
+        url: url,
+        dataType: 'json',
+        success: function(data) {
+          console.log('yes');
+          el.replaceWith(data.partial);
+          return false;
+        },
+        error: function(data) {
+          console.log('no');
+          return false;
+        }
+      });
+    });
+  });
 });
