fd4e5f13c36e0bbbf254b840eff2e74d186ff0a1
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2017-07-14 22:45:20 +0000

    Added posts comments viewing for non logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-
+  root to: 'pages#home'
+  get 'users/posts/:id', to: 'users/posts#show'
   # /users
   namespace :users do
     authenticate :user do
-      resources :posts
+      resources :posts, except: [:show]
       match 'posts/:id/vote/:type', to: 'posts#vote', via: :post
       namespace :posts do
         resources :comments
