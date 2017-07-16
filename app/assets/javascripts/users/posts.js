977d071d2d460bb78a33c8225e5cb377a20e9a0c
Sean Kennedy <cspheregreen@gmail.com>
2017-07-15 20:54:26 -0500

    fix post voting dupe js event and change comment votes to use anchor

--- app/assets/javascripts/users/posts.js
+++ app/assets/javascripts/users/posts.js
@@ -3,7 +3,8 @@
 // AJAX for post voting
 $(function() {
   $.each(['up', 'down'], function(i, o) {
-    $(document).on('click', "button.post-vote-" + o, function() {
+    $(document).on('click', ".post-vote-" + o, function(e) {
+      e.preventDefault();
       var el = $(this).closest(".post-votes");
       var id = el.data("id");
       var url = "/users/posts/" + id + "/vote/" + o;
@@ -12,12 +13,10 @@ $(function() {
         url: url,
         dataType: 'json',
         success: function(data) {
-          console.log('yes');
           el.replaceWith(data.partial);
           return false;
         },
         error: function(data) {
-          console.log('no');
           return false;
         }
       });
--- app/assets/javascripts/users/posts/comments.js
+++ app/assets/javascripts/users/posts/comments.js
@@ -13,13 +13,11 @@ $(function() {
       data: data,
       dataType: 'json',
       success: function(data) {
-        console.log('yes');
         form.replaceWith(data.form);
         $('#post-comments').replaceWith(data.comments);
         return false;
       },
       error: function(data) {
-        console.log('no');
         return false;
       }
     });
@@ -27,23 +25,21 @@ $(function() {
 
   // AJAX for comment voting
   $.each(['up', 'down'], function(i, o) {
-    $(document).on('click', 'button.comment-vote-' + o, function() {
+    $(document).on('click', '.comment-vote-' + o, function(e) {
+      e.preventDefault();
       var el = $(this).closest('.comment-votes');
       var post_id = $(this).closest('.post').data('id');
       var comment_id = el.data('id');
-      console.log(post_id);
       var url = '/users/posts/' + post_id + '/comments/' + comment_id + '/vote/' + o;
       return $.ajax({
         type: 'POST',
         url: url,
         dataType: 'json',
         success: function(data) {
-          console.log('yes');
           el.replaceWith(data.partial);
           return false;
         },
         error: function(data) {
-          console.log('no');
           return false;
         }
       });
