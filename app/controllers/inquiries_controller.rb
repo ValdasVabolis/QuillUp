aa02447efedcafdd6e82d04d51b1bd91b0704d75
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-11 02:21:55 +0300

    A few minor adjustments

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -10,7 +10,6 @@ class InquiriesController < ApplicationController
       name = current_user.username
       email = current_user.email
       message = params[:inquiry][:message]
-
       InquiryMailer.send_inquiry(name, email, message).deliver
       flash[:notice] = 'Message sent succesfully!'
       redirect_to root_url
@@ -23,10 +22,6 @@ class InquiriesController < ApplicationController
   private
 
   def inquiry_params
-    params.require(:inquiry).permit(
-      :name, 
-      :email, 
-      :message
-    )
+    params.require(:inquiry).permit(:message)
   end
 end
