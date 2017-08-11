43080e40ae72222a5cecfb1848b4e3513b43e475
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-12 00:21:47 +0300

    Fixed public profile tabs not rendering

--- app/helpers/account_helper.rb
+++
@@ -1,9 +0,0 @@
-module AccountHelper
-  def account_tab(name)
-    path = "/account/#{name}"
-    text = name.to_s.capitalize
-    link = link_to text, path
-    cls = request.path == path ? 'selected' : ''
-    content_tag :li, link, class: cls
-  end
-end
