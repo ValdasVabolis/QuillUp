9bfcd5a547efa0549c102c18f804712b4f97956a
Sean Kennedy <cspheregreen@gmail.com>
2017-08-11 04:56:36 +0300

    add selected class back to tab nav

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -3,6 +3,7 @@ module AccountHelper
     path = "/account/#{name}"
     text = name.to_s.capitalize
     link = link_to text, path
-    content_tag :li, link
+    cls = request.path == path ? 'selected' : '' 
+    content_tag :li, link, class: cls
   end
 end
