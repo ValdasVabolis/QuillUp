67f79779378726430f0452f2bb81282f6243fc5f
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-25 16:41:30 +0300

    Added about me for public and private profiles and a helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,12 @@ module UserHelper
   def user_score_sum(user, type)
     user.send("#{type}s").map(&:score).sum
   end
+
+  def user_about_me(user)
+    if user.about.nil?
+      render html: '<h2>About me</h2><p>User does not have an about me yet.</p>'.html_safe
+    else
+      render html: "<h2>About me</h2><p>#{user.about}</p>".html_safe
+    end
+  end
 end
