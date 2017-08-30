53a5c37ceb130a6f224bf5a29dc15170b240b4ff
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-08-30 19:39:34 +0300

    Reworked about me page text limiter to use new module

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,32 +1,9 @@
 $(document).on('turbolinks:load', function() {
- /* function loadedOnPage(name) {
-    return $('#' + name + '-content').length > 0;
-  }
-  function showCharacterLimit(limit) {
-    var text_area = $('#about-me');
-    var char_counter = $('.char-counter p');
-    var length = text_area.val().length;
-    var counter = length + '/' + limit; 
-    char_counter.html(counter);
-
-    function updateCounter() {
-      length = text_area.val().length;
-      counter = length + '/' + limit;
-      char_counter.html(counter);
-    }
-
-    text_area.bind('keyup keydown', function(e) {
-      updateCounter();
-      if(length >= limit) {
-        if(e.key !== 'Backspace') {
-          this.value = this.value.substr(0, limit);
-          return false;
-        }
-      }
+  if(('#account-content').length > 0) {
+    Text.limit({
+      text_selector: '#about-me',
+      count_selector: '#about-me-counter',
+      limit: 200
     });
-
   }
-  if(loadedOnPage('account')) {
-    showCharacterLimit(200);
-  }*/
 });
