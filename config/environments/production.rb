6966a54335247129954d903bab21f798cc552f92
Sean Kennedy <cspheregreen@gmail.com>
2017-09-15 17:50:44 -0500

    i fucked up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = 'www.quillup.com'
+  config.action_mailer.default_url_options = {
+    host: 'www.quillup.com'
+  }
 end
