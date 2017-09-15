d139cbe91001a29ff44e0c807f30ec3477a63483
Sean Kennedy <cspheregreen@gmail.com>
2017-09-15 17:20:38 -0500

    db wip

--- db/migrate/20170808010054_remove_comment_index_from_answers.rb
+++ db/migrate/20170808010054_remove_comment_index_from_answers.rb
@@ -1,5 +1,5 @@
 class RemoveCommentIndexFromAnswers < ActiveRecord::Migration[5.1]
   def change
-  	remove_reference :answers, :comment, foreign_key: true, index: true
+  	remove_reference :answers, :comment
   end
 end
