0c19311fe89b4f2f258682446d5ede733d306269
Sean Kennedy <cspheregreen@gmail.com>
2017-09-15 17:27:21 -0500

    db wip

--- db/migrate/20170814045953_add_time_stamps_to_comments.rb
+++ db/migrate/20170814045953_add_time_stamps_to_comments.rb
@@ -1,8 +1,8 @@
 class AddTimeStampsToComments < ActiveRecord::Migration[5.1]
   def change
     change_table :comments do |t|
-      t.datetime :created_at, null: false, default: false
-      t.datetime :updated_at, null: false, default: false
+      t.datetime :created_at, null: false, default: Time.now
+      t.datetime :updated_at, null: false, default: Time.now
     end
   end
 end
