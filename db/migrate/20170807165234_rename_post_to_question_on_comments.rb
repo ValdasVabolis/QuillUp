bd064da1ed92d726f1c483d70a1c4bd90e576955
Sean Kennedy <cspheregreen@gmail.com>
2017-09-15 16:53:22 -0500

    db wip

--- db/migrate/20170807165234_rename_post_to_question_on_comments.rb
+++ db/migrate/20170807165234_rename_post_to_question_on_comments.rb
@@ -1,6 +1,6 @@
 class RenamePostToQuestionOnComments < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :comments, :post
+    remove_reference :comments, :question
     add_reference :comments, :question, foreign_key: true, index: true
   end
 end
