23f99ce4e14977ad59dccc89b83da4ea44d9d1e6
Sean Kennedy <cspheregreen@gmail.com>
2017-09-17 17:17:48 -0500

    fix some stuff and things

--- spec/acceptance/user_sign_up_spec.rb
+++ spec/acceptance/user_sign_up_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 feature "sign in process", type: :feature, js: true do
-  
+  background do
+    @user = build(:user)
+  end
 
   scenario 'user registration' do
-    visit 'users/sign_up'
+    visit root_path
+
+    click_link 'Sign up'
 
     within("#new_user") do
-      fill_in 'Username', with: @user.email
+      fill_in 'Username', with: @user.username
+      fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
+      fill_in 'Confirm Password', with: @user.password_confirmation
     end
-    click_button 'Sign in'
-    expect(page).to have_content 'Signed in successfully.'
-    click_link 'Sign out'
-    expect(page).to have_content 'Signed out successfully.'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.'
+
+    registered_user = User.find_by_email(@user.email)
+
+    visit "users/confirmation?confirmation_token=#{registered_user.confirmation_token}"
+
+    expect(page).to have_content 'Your email address has been successfully confirmed.'
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do |f|
   factory :user do |u|
+    u.sequence(:username) { |n| "user#{n}" }
     u.sequence(:email) { |n| "email#{n}@example.com" }
     u.password "password"
     u.password_confirmation "password"
