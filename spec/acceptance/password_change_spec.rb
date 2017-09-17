29e98f492ec72c41fcb0a6321c45c9c9da0f252c
valdasvabolis <thisisvaldasvabolis@gmail.com>
2017-09-17 19:11:44 +0300

    Acceptance tests for user password change

---
+++ spec/acceptance/password_change_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature 'password change process', type: :feature, js: true do
+  background do
+    @user = create(:user, {
+      username: 'SeanKennedy',
+      email: 'user@example.com',
+      password: 'password'
+    })
+  end
+
+  scenario 'user signs in, changes password, signs out, signs in with new password' do
+    visit 'users/sign_in'
+
+    within '#new_user' do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+    end
+
+    click_button 'Sign in'
+
+    visit '/account/settings'
+
+    within '.edit_user' do
+      fill_in 'user_current_password', with: @user.password
+      fill_in 'user_password', with: 'newpassword'
+      fill_in 'user_password_confirmation', with: 'newpassword'
+    end
+
+    click_button 'Update'
+
+    # TODO: fetch the updated password from database, to check if the actual db update works
+    @user.password = 'newpassword'
+
+    expect(page).to have_content 'Your account has been updated successfully.'
+
+    click_link 'Sign out'
+
+    visit 'users/sign_in'
+
+    within("#new_user") do
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+    end
+    click_button 'Sign in'
+    expect(page).to have_content 'Signed in successfully.'
+  end
+end
